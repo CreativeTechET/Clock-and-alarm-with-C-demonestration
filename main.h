f67743b5b9a782425fd12bfce266d75bf524ced0
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-06-18 20:42:55 +0300

    clocksys init

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef Main
+#define Main
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <curses.h>
+
+void fclear(void);
+void fheader(void);
+void clock(void);
+void start(void);
+
+#endif
